b9d71a676612e0da9db5f6cd4cd44a3e93acc893
lakshmi-p15 <lakshmi.pavuluri15@gmail.com>
2022-09-05 15:16:45 -0700

    completed Dice Roller implementation

--- app/src/main/java/com/example/dicerollerkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerkotlinapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.dicerollerkotlinapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dicerollerkotlinapp.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollButton.setOnClickListener {
+            rillDice()
+        }
+    }
+
+    private fun rillDice() {
+        val drawableResource = when(Random(System.nanoTime()).nextInt(6) + 1){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.diceImage.setImageResource(drawableResource)
     }
 }
